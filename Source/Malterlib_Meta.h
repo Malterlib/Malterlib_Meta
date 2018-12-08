6bc45f6f6a41548f03c2ceeec769c51e3a1fa2f3
Erik Olofsson <erik.olofsson@favro.com>
2018-12-08 19:48:49 +0100

    [Malterlib/Meta] Refactor namespaces

--- Source/Malterlib_Meta.h
+++ Source/Malterlib_Meta.h
@@ -1,222 +1,220 @@
-﻿// Copyright © 2015 Hansoft AB 
+// Copyright © 2015 Hansoft AB 
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
 #pragma once
 
+namespace NMib::NMeta
+{
+	/////////////
+	// Indices
+
+	template <mint...>
+	struct TCIndices
+	{
+	};
+
+	namespace NPrivate
+	{
+		template <mint t_CurrentIndex, typename t_CIndices, mint t_EndIndex>
+		struct TCMakeConsecutiveIndicesImp;
+
+		template <mint t_CurrentIndex, mint... _Indices, mint t_EndIndex>
+		struct TCMakeConsecutiveIndicesImp<t_CurrentIndex, TCIndices<_Indices...>, t_EndIndex>
+		{
+			typedef typename TCMakeConsecutiveIndicesImp<t_CurrentIndex+1, TCIndices<_Indices..., t_CurrentIndex>, t_EndIndex>::CType CType;
+		};
+
+		template <mint t_EndIndex, mint... _Indices>
+		struct TCMakeConsecutiveIndicesImp<t_EndIndex, TCIndices<_Indices...>, t_EndIndex>
+		{
+			typedef TCIndices<_Indices...> CType;
+		};
+	}
+
+	template <mint t_EndIndex, mint t_StartIndex = 0>
+	struct TCMakeConsecutiveIndices
+	{
+		static_assert(t_StartIndex <= t_EndIndex, "Start index cannot be larger than the end index");
+		typedef typename NPrivate::TCMakeConsecutiveIndicesImp<t_StartIndex, TCIndices<>, t_EndIndex>::CType CType;
+	};
+
+	/////////////
+	// All is true
+
+	namespace NPrivate
+	{
+		template <bool... tp_CToCheck> struct TCAllIsTrueImp;
+
+		template <bool t_bToCheck, bool... tp_CToCheck>
+		struct TCAllIsTrueImp<t_bToCheck, tp_CToCheck...>
+		{
+			static constexpr bool mc_Value = t_bToCheck && TCAllIsTrueImp<tp_CToCheck...>::mc_Value;
+		};
+
+		template <>
+		struct TCAllIsTrueImp<>
+		{
+			static constexpr bool mc_Value = true;
+		};
+	}
+
+	template <bool... tp_CToCheck>
+	class TCAllIsTrue
+		: public NTraits::TCCompileTimeConstant<bool, NPrivate::TCAllIsTrueImp<tp_CToCheck...>::mc_Value>
+	{
+	};
+
+
+	/////////////
+	// Type list
+
+	template <typename... tp_CCTypes>
+	struct TCTypeList
+	{
+	};
+
+	/////////////
+	// Type list concat
+
+	template <typename t_CList0, typename t_CList1>
+	struct TCTypeList_Concat;
+
+	template <typename... tp_CParams0, typename... tp_CParams1>
+	struct TCTypeList_Concat<TCTypeList<tp_CParams0...>, TCTypeList<tp_CParams1...>>
+	{
+		typedef TCTypeList<tp_CParams0..., tp_CParams1...> CType;
+	};
+
+	/////////////
+	// Type list len
+
+	template <typename t_CType>
+	class TCTypeList_Len;
+
+	// Support any qualifiers
+	template <typename t_CType>
+	class TCTypeList_Len<t_CType const>
+		: public TCTypeList_Len<t_CType>
+	{
+	};
+	template <typename t_CType>
+	class TCTypeList_Len<t_CType volatile>
+		: public TCTypeList_Len<t_CType>
+	{
+	};
+	template <typename t_CType>
+	class TCTypeList_Len<t_CType const volatile>
+		: public TCTypeList_Len<t_CType>
+	{
+	};
+
+	template <typename... tp_CCTypes>
+	class TCTypeList_Len<TCTypeList<tp_CCTypes...>>
+		: public NTraits::TCCompileTimeConstant<mint, sizeof...(tp_CCTypes)>
+	{
+	};
+
+	/////////////
+	// Type list get
+
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_Get;
+
+	// Copy qualifiers of type list
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_Get<t_Index, t_CType const>
+	{
+	public:
+		typedef typename NTraits::TCAddConst<typename TCTypeList_Get<t_Index, t_CType>::CType>::CType CType;
+	};
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_Get<t_Index, t_CType volatile>
+	{
+	public:
+		typedef typename NTraits::TCAddVolatile<typename TCTypeList_Get<t_Index, t_CType>::CType>::CType CType;
+	};
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_Get<t_Index, t_CType const volatile>
+	{
+	public:
+		typedef typename NTraits::TCAddConstVolatile<typename TCTypeList_Get<t_Index, t_CType>::CType>::CType CType;
+	};
+
+	template <mint t_Index>
+	class TCTypeList_Get<t_Index, TCTypeList<> >
+	{
+	public:
+		static_assert(t_Index == 0 && t_Index != 0, "TCTypeList_Get index out of range");
+	};
+
+	template <typename t_CHeadType, typename... tp_CCTypes>
+	class TCTypeList_Get<0, NMeta::TCTypeList<t_CHeadType, tp_CCTypes...>>
+	{
+	public:
+		typedef t_CHeadType CType;
+	};
+
+	template <mint t_Index, typename t_CHeadType, typename... tp_CCTypes>
+	class TCTypeList_Get<t_Index, TCTypeList<t_CHeadType, tp_CCTypes...>>
+	{
+	public:
+		typedef typename TCTypeList_Get<t_Index-1, TCTypeList<tp_CCTypes...>>::CType CType;
+	};
+
+
+
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_GetOrVoid;
+
+	// Copy qualifiers of type list
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_GetOrVoid<t_Index, t_CType const>
+	{
+	public:
+		typedef typename NTraits::TCAddConst<typename TCTypeList_GetOrVoid<t_Index, t_CType>::CType>::CType CType;
+	};
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_GetOrVoid<t_Index, t_CType volatile>
+	{
+	public:
+		typedef typename NTraits::TCAddVolatile<typename TCTypeList_GetOrVoid<t_Index, t_CType>::CType>::CType CType;
+	};
+	template <mint t_Index, typename t_CType>
+	class TCTypeList_GetOrVoid<t_Index, t_CType const volatile>
+	{
+	public:
+		typedef typename NTraits::TCAddConstVolatile<typename TCTypeList_GetOrVoid<t_Index, t_CType>::CType>::CType CType;
+	};
+
+	template <mint t_Index>
+	class TCTypeList_GetOrVoid<t_Index, TCTypeList<> >
+	{
+	public:
+		typedef void CType;
+	};
+
+	template <typename t_CHeadType, typename... tp_CCTypes>
+	class TCTypeList_GetOrVoid<0, NMeta::TCTypeList<t_CHeadType, tp_CCTypes...>>
+	{
+	public:
+		typedef t_CHeadType CType;
+	};
+
+	template <mint t_Index, typename t_CHeadType, typename... tp_CCTypes>
+	class TCTypeList_GetOrVoid<t_Index, TCTypeList<t_CHeadType, tp_CCTypes...>>
+	{
+	public:
+		typedef typename TCTypeList_GetOrVoid<t_Index-1, TCTypeList<tp_CCTypes...>>::CType CType;
+	};
+}
+
 namespace NMib
 {
-	namespace NMeta
-	{
-		/////////////
-		// Indices
-		
-		template <mint...> 
-		struct TCIndices
-		{
-		};
-		
-		namespace NPrivate
-		{
-			template <mint t_CurrentIndex, typename t_CIndices, mint t_EndIndex>
-			struct TCMakeConsecutiveIndicesImp;
-			
-			template <mint t_CurrentIndex, mint... _Indices, mint t_EndIndex>
-			struct TCMakeConsecutiveIndicesImp<t_CurrentIndex, TCIndices<_Indices...>, t_EndIndex>
-			{
-				typedef typename TCMakeConsecutiveIndicesImp<t_CurrentIndex+1, TCIndices<_Indices..., t_CurrentIndex>, t_EndIndex>::CType CType;
-			};
-			
-			template <mint t_EndIndex, mint... _Indices>
-			struct TCMakeConsecutiveIndicesImp<t_EndIndex, TCIndices<_Indices...>, t_EndIndex>
-			{
-				typedef TCIndices<_Indices...> CType;
-			};
-		}
-		
-		template <mint t_EndIndex, mint t_StartIndex = 0>
-		struct TCMakeConsecutiveIndices
-		{
-			static_assert(t_StartIndex <= t_EndIndex, "Start index cannot be larger than the end index");
-			typedef typename NPrivate::TCMakeConsecutiveIndicesImp<t_StartIndex, TCIndices<>, t_EndIndex>::CType CType;
-		};
-
-		/////////////
-		// All is true
-		
-		namespace NPrivate
-		{
-			template <bool... tp_CToCheck> struct TCAllIsTrueImp;
-			
-			template <bool t_bToCheck, bool... tp_CToCheck>
-			struct TCAllIsTrueImp<t_bToCheck, tp_CToCheck...>
-			{
-				static constexpr bool mc_Value = t_bToCheck && TCAllIsTrueImp<tp_CToCheck...>::mc_Value;
-			};
-			
-			template <>
-			struct TCAllIsTrueImp<>
-			{
-				static constexpr bool mc_Value = true;
-			};
-		}
-
-		template <bool... tp_CToCheck> 
-		class TCAllIsTrue 
-			: public NTraits::TCCompileTimeConstant<bool, NPrivate::TCAllIsTrueImp<tp_CToCheck...>::mc_Value>
-		{
-		};
-
-
-		/////////////
-		// Type list
-		
-		template <typename... tp_CCTypes>
-		struct TCTypeList
-		{
-		};
-
-		/////////////
-		// Type list concat
-		
-		template <typename t_CList0, typename t_CList1>
-		struct TCTypeList_Concat;
-		
-		template <typename... tp_CParams0, typename... tp_CParams1>
-		struct TCTypeList_Concat<TCTypeList<tp_CParams0...>, TCTypeList<tp_CParams1...>>
-		{
-			typedef TCTypeList<tp_CParams0..., tp_CParams1...> CType;
-		};
-		
-		/////////////
-		// Type list len
-		
-		template <typename t_CType>
-		class TCTypeList_Len;
-		
-		// Support any qualifiers
-		template <typename t_CType>
-		class TCTypeList_Len<t_CType const>
-			: public TCTypeList_Len<t_CType>
-		{
-		};
-		template <typename t_CType>
-		class TCTypeList_Len<t_CType volatile>
-			: public TCTypeList_Len<t_CType>
-		{
-		};
-		template <typename t_CType>
-		class TCTypeList_Len<t_CType const volatile>
-			: public TCTypeList_Len<t_CType>
-		{
-		};
-		
-		template <typename... tp_CCTypes>
-		class TCTypeList_Len<TCTypeList<tp_CCTypes...>>
-			: public NTraits::TCCompileTimeConstant<mint, sizeof...(tp_CCTypes)>
-		{
-		};
-
-		/////////////
-		// Type list get
-		
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_Get;
-		
-		// Copy qualifiers of type list
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_Get<t_Index, t_CType const>
-		{
-		public:
-			typedef typename NTraits::TCAddConst<typename TCTypeList_Get<t_Index, t_CType>::CType>::CType CType;
-		};
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_Get<t_Index, t_CType volatile>
-		{
-		public:
-			typedef typename NTraits::TCAddVolatile<typename TCTypeList_Get<t_Index, t_CType>::CType>::CType CType;
-		};
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_Get<t_Index, t_CType const volatile>
-		{
-		public:
-			typedef typename NTraits::TCAddConstVolatile<typename TCTypeList_Get<t_Index, t_CType>::CType>::CType CType;
-		};
-		
-		template <mint t_Index>
-		class TCTypeList_Get<t_Index, TCTypeList<> >
-		{
-		public:
-			static_assert(t_Index == 0 && t_Index != 0, "TCTypeList_Get index out of range");
-		};
-		
-		template <typename t_CHeadType, typename... tp_CCTypes>
-		class TCTypeList_Get<0, NMeta::TCTypeList<t_CHeadType, tp_CCTypes...>>
-		{
-		public:
-			typedef t_CHeadType CType;
-		};
-
-		template <mint t_Index, typename t_CHeadType, typename... tp_CCTypes>
-		class TCTypeList_Get<t_Index, TCTypeList<t_CHeadType, tp_CCTypes...>>
-		{
-		public:
-			typedef typename TCTypeList_Get<t_Index-1, TCTypeList<tp_CCTypes...>>::CType CType;
-		};
-
-		
-		
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_GetOrVoid;
-		
-		// Copy qualifiers of type list
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_GetOrVoid<t_Index, t_CType const>
-		{
-		public:
-			typedef typename NTraits::TCAddConst<typename TCTypeList_GetOrVoid<t_Index, t_CType>::CType>::CType CType;
-		};
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_GetOrVoid<t_Index, t_CType volatile>
-		{
-		public:
-			typedef typename NTraits::TCAddVolatile<typename TCTypeList_GetOrVoid<t_Index, t_CType>::CType>::CType CType;
-		};
-		template <mint t_Index, typename t_CType>
-		class TCTypeList_GetOrVoid<t_Index, t_CType const volatile>
-		{
-		public:
-			typedef typename NTraits::TCAddConstVolatile<typename TCTypeList_GetOrVoid<t_Index, t_CType>::CType>::CType CType;
-		};
-		
-		template <mint t_Index>
-		class TCTypeList_GetOrVoid<t_Index, TCTypeList<> >
-		{
-		public:
-			typedef void CType;
-		};
-		
-		template <typename t_CHeadType, typename... tp_CCTypes>
-		class TCTypeList_GetOrVoid<0, NMeta::TCTypeList<t_CHeadType, tp_CCTypes...>>
-		{
-		public:
-			typedef t_CHeadType CType;
-		};
-
-		template <mint t_Index, typename t_CHeadType, typename... tp_CCTypes>
-		class TCTypeList_GetOrVoid<t_Index, TCTypeList<t_CHeadType, tp_CCTypes...>>
-		{
-		public:
-			typedef typename TCTypeList_GetOrVoid<t_Index-1, TCTypeList<tp_CCTypes...>>::CType CType;
-		};
-		
-	}
-	
 	template <typename... tfp_CTypes>
-	void fg_Swallow(tfp_CTypes &&...) noexcept 
+	void fg_Swallow(tfp_CTypes &&...) noexcept
 	{
 	}
 }
-
 
 #ifndef DMibPNoShortCuts
 	using namespace NMib::NMeta;
--- Source/Malterlib_Meta_TypeListUtils.h
+++ Source/Malterlib_Meta_TypeListUtils.h
@@ -1,197 +1,191 @@
-﻿// Copyright © 2015 Hansoft AB 
+// Copyright © 2015 Hansoft AB 
 // Distributed under the MIT license, see license text in LICENSE.Malterlib
 
 #pragma once
 #include "Malterlib_Meta.h"
 
-namespace NMib
+namespace NMib::NMeta
 {
-	namespace NMeta
-	{
+	/////////////
+	// Type list promote lvalue ref
 
-		/////////////
-		// Type list promote lvalue ref
-		
-		namespace NPrivate
+	namespace NPrivate
+	{
+		template <typename t_CTypes, typename t_CSourceList, mint t_Current, mint t_End>
+		struct TCTypeListp_CromoteLValueRefImp;
+
+		template <typename... tp_CCTypes, typename t_CSourceList, mint t_Current, mint t_End>
+		struct TCTypeListp_CromoteLValueRefImp<TCTypeList<tp_CCTypes...>, t_CSourceList, t_Current, t_End>
 		{
-			template <typename t_CTypes, typename t_CSourceList, mint t_Current, mint t_End>
-			struct TCTypeListp_CromoteLValueRefImp;
-			
-			template <typename... tp_CCTypes, typename t_CSourceList, mint t_Current, mint t_End>
-			struct TCTypeListp_CromoteLValueRefImp<TCTypeList<tp_CCTypes...>, t_CSourceList, t_Current, t_End>
-			{
-				typedef typename NTraits::TCRemoveReference<t_CSourceList>::CType CSourceListNoRef;
-				typedef typename 
-					TCTypeListp_CromoteLValueRefImp
+			typedef typename NTraits::TCRemoveReference<t_CSourceList>::CType CSourceListNoRef;
+			typedef typename
+				TCTypeListp_CromoteLValueRefImp
+				<
+					TCTypeList
 					<
-						TCTypeList
+						tp_CCTypes...
+						, typename TCChooseType
 						<
-							tp_CCTypes...
+							NTraits::TCIsLValueReference<t_CSourceList>::mc_Value
+							, typename NTraits::TCAddLValueReference<typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType>::CType
+#if 1
+							, typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType
+#else
 							, typename TCChooseType
 							<
-								NTraits::TCIsLValueReference<t_CSourceList>::mc_Value
-								, typename NTraits::TCAddLValueReference<typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType>::CType
-#if 1
+								NTraits::TCIsRValueReference<t_CSourceList>::mc_Value
+								, typename NTraits::TCAddRValueReference<typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType>::CType
 								, typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType
-#else
-								, typename TCChooseType
-								<
-									NTraits::TCIsRValueReference<t_CSourceList>::mc_Value
-									, typename NTraits::TCAddRValueReference<typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType>::CType
-									, typename TCTypeList_Get<t_Current, CSourceListNoRef>::CType
-								>::CType
-#endif
 							>::CType
-						>
-						, t_CSourceList
-						, t_Current+1
-						, t_End
-					>::CType CType
-				;
-			};
-			
-			template <typename... tp_CCTypes, typename t_CSourceList, mint t_End>
-			struct TCTypeListp_CromoteLValueRefImp<TCTypeList<tp_CCTypes...>, t_CSourceList, t_End, t_End>
-			{
-				typedef TCTypeList<tp_CCTypes...> CType;
-			};
-		}
-		
-		template <typename t_CSourceList, mint t_End = TCTypeList_Len<typename NTraits::TCRemoveReference<t_CSourceList>::CType>::mc_Value, mint t_Start = 0>
-		struct TCTypeList_MakePromotedLValueRef
-		{
-			static_assert(t_Start <= t_End, "t_Start needs to be less than end");
-			static_assert(t_End <= TCTypeList_Len<typename NTraits::TCRemoveReference<t_CSourceList>::CType>::mc_Value, "t_End is larger than size of list");
-			typedef typename NPrivate::TCTypeListp_CromoteLValueRefImp<TCTypeList<>, t_CSourceList, t_Start, t_End>::CType CType;
+#endif
+						>::CType
+					>
+					, t_CSourceList
+					, t_Current+1
+					, t_End
+				>::CType CType
+			;
 		};
 
-		/////////////
-		// Type list is convertible
-
-		
-		namespace NPrivate
+		template <typename... tp_CCTypes, typename t_CSourceList, mint t_End>
+		struct TCTypeListp_CromoteLValueRefImp<TCTypeList<tp_CCTypes...>, t_CSourceList, t_End, t_End>
 		{
-			template <bool t_bSameLength, typename t_CFromList, typename t_CToList>
-			struct TCTypeList_IsConvertibleImp : public NTraits::CCompileTimeFalse 
-			{
-			};
-			
-			template <typename t_CFromHead, typename... tp_CFromTypes, typename t_CToHead, typename... tp_CToTypes>
-			struct TCTypeList_IsConvertibleImp<true, TCTypeList<t_CFromHead, tp_CFromTypes...>, TCTypeList<t_CToHead, tp_CToTypes...>>
-				: public NTraits::TCCompileTimeConstant
-				<
-					bool
-					, NTraits::TCIsConvertible<t_CFromHead, t_CToHead>::mc_Value
-					&& TCTypeList_IsConvertibleImp<true, TCTypeList<tp_CFromTypes...>, TCTypeList<tp_CToTypes...> >::mc_Value
-				> 
-			{
-			};
-			
-			template <>
-			struct TCTypeList_IsConvertibleImp<true, TCTypeList<>, TCTypeList<>>
-				: public NTraits::CCompileTimeTrue
-			{
-			};
-			
-		}
-
-		template <typename t_CFromList, typename t_CToList>
-		struct TCTypeList_IsConvertible
-			: public NPrivate::TCTypeList_IsConvertibleImp
-			<
-				TCTypeList_Len<typename NTraits::TCRemoveReference<t_CFromList>::CType>::mc_Value
-				== TCTypeList_Len<typename NTraits::TCRemoveReference<t_CToList>::CType>::mc_Value // Same length
-				, typename TCTypeList_MakePromotedLValueRef<t_CFromList>::CType
-				, t_CToList
-			>
-		{
-			static_assert(!NTraits::TCIsReference<t_CToList>::mc_Value, "To list cannot be reference");
+			typedef TCTypeList<tp_CCTypes...> CType;
 		};
-
-		/////////////
-		// Type list is constructible
-
-		namespace NPrivate
-		{
-			template <bool t_bSameLength, typename t_CFromList, typename t_CToList>
-			struct TCTypeList_IsConstructibleImp : public NTraits::CCompileTimeFalse 
-			{
-			};
-			
-			template <typename t_CFromHead, typename... tp_CFromTypes, typename t_CToHead, typename... tp_CToTypes>
-			struct TCTypeList_IsConstructibleImp<true, TCTypeList<t_CFromHead, tp_CFromTypes...>, TCTypeList<t_CToHead, tp_CToTypes...>>
-				: public NTraits::TCCompileTimeConstant
-				<
-					bool
-					, NTraits::TCIsConstructorCallableWith<t_CToHead, void (t_CFromHead)>::mc_Value
-					&& TCTypeList_IsConstructibleImp<true, TCTypeList<tp_CFromTypes...>, TCTypeList<tp_CToTypes...> >::mc_Value
-				> 
-			{
-			};
-			
-			template <>
-			struct TCTypeList_IsConstructibleImp<true, TCTypeList<>, TCTypeList<>>
-				: public NTraits::CCompileTimeTrue
-			{
-			};
-			
-		}
-
-		template <typename t_CFromList, typename t_CToList>
-		struct TCTypeList_IsConstructible
-			: public NPrivate::TCTypeList_IsConstructibleImp
-			<
-				TCTypeList_Len<typename NTraits::TCRemoveReference<t_CFromList>::CType>::mc_Value
-				== TCTypeList_Len<typename NTraits::TCRemoveReference<t_CToList>::CType>::mc_Value // Same length
-				, typename TCTypeList_MakePromotedLValueRef<t_CFromList>::CType
-				, t_CToList
-			>
-		{
-			static_assert(!NTraits::TCIsReference<t_CToList>::mc_Value, "To list cannot be reference");
-		};
-
-		/////////////
-		// Type list is assignable
-
-		namespace NPrivate
-		{
-			template <bool t_bSameLength, typename t_CFromList, typename t_CToList>
-			struct TCTypeList_IsAssignableImp : public NTraits::CCompileTimeFalse 
-			{
-			};
-			
-			template <typename t_CFromHead, typename... tp_CFromTypes, typename t_CToHead, typename... tp_CToTypes>
-			struct TCTypeList_IsAssignableImp<true, TCTypeList<t_CFromHead, tp_CFromTypes...>, TCTypeList<t_CToHead, tp_CToTypes...>>
-				: public NTraits::TCCompileTimeConstant
-				<
-					bool
-					, NTraits::TCIsOperatorCallableWith_Assign<t_CToHead, void (t_CFromHead)>::mc_Value
-					&& TCTypeList_IsAssignableImp<true, TCTypeList<tp_CFromTypes...>, TCTypeList<tp_CToTypes...> >::mc_Value
-				> 
-			{
-			};
-			
-			template <>
-			struct TCTypeList_IsAssignableImp<true, TCTypeList<>, TCTypeList<>>
-				: public NTraits::CCompileTimeTrue
-			{
-			};
-			
-		}
-
-		template <typename t_CFromList, typename t_CToList>
-		struct TCTypeList_IsAssignable
-			: public NPrivate::TCTypeList_IsAssignableImp
-			<
-				TCTypeList_Len<typename NTraits::TCRemoveReference<t_CFromList>::CType>::mc_Value
-				== TCTypeList_Len<typename NTraits::TCRemoveReference<t_CToList>::CType>::mc_Value // Same length
-				, typename TCTypeList_MakePromotedLValueRef<t_CFromList>::CType
-				, t_CToList
-			>
-		{
-			static_assert(!NTraits::TCIsReference<t_CToList>::mc_Value, "To list cannot be reference");
-		};
-		
 	}
-}
 
+	template <typename t_CSourceList, mint t_End = TCTypeList_Len<typename NTraits::TCRemoveReference<t_CSourceList>::CType>::mc_Value, mint t_Start = 0>
+	struct TCTypeList_MakePromotedLValueRef
+	{
+		static_assert(t_Start <= t_End, "t_Start needs to be less than end");
+		static_assert(t_End <= TCTypeList_Len<typename NTraits::TCRemoveReference<t_CSourceList>::CType>::mc_Value, "t_End is larger than size of list");
+		typedef typename NPrivate::TCTypeListp_CromoteLValueRefImp<TCTypeList<>, t_CSourceList, t_Start, t_End>::CType CType;
+	};
+
+	/////////////
+	// Type list is convertible
+
+
+	namespace NPrivate
+	{
+		template <bool t_bSameLength, typename t_CFromList, typename t_CToList>
+		struct TCTypeList_IsConvertibleImp : public NTraits::CCompileTimeFalse
+		{
+		};
+
+		template <typename t_CFromHead, typename... tp_CFromTypes, typename t_CToHead, typename... tp_CToTypes>
+		struct TCTypeList_IsConvertibleImp<true, TCTypeList<t_CFromHead, tp_CFromTypes...>, TCTypeList<t_CToHead, tp_CToTypes...>>
+			: public NTraits::TCCompileTimeConstant
+			<
+				bool
+				, NTraits::TCIsConvertible<t_CFromHead, t_CToHead>::mc_Value
+				&& TCTypeList_IsConvertibleImp<true, TCTypeList<tp_CFromTypes...>, TCTypeList<tp_CToTypes...> >::mc_Value
+			>
+		{
+		};
+
+		template <>
+		struct TCTypeList_IsConvertibleImp<true, TCTypeList<>, TCTypeList<>>
+			: public NTraits::CCompileTimeTrue
+		{
+		};
+
+	}
+
+	template <typename t_CFromList, typename t_CToList>
+	struct TCTypeList_IsConvertible
+		: public NPrivate::TCTypeList_IsConvertibleImp
+		<
+			TCTypeList_Len<typename NTraits::TCRemoveReference<t_CFromList>::CType>::mc_Value
+			== TCTypeList_Len<typename NTraits::TCRemoveReference<t_CToList>::CType>::mc_Value // Same length
+			, typename TCTypeList_MakePromotedLValueRef<t_CFromList>::CType
+			, t_CToList
+		>
+	{
+		static_assert(!NTraits::TCIsReference<t_CToList>::mc_Value, "To list cannot be reference");
+	};
+
+	/////////////
+	// Type list is constructible
+
+	namespace NPrivate
+	{
+		template <bool t_bSameLength, typename t_CFromList, typename t_CToList>
+		struct TCTypeList_IsConstructibleImp : public NTraits::CCompileTimeFalse
+		{
+		};
+
+		template <typename t_CFromHead, typename... tp_CFromTypes, typename t_CToHead, typename... tp_CToTypes>
+		struct TCTypeList_IsConstructibleImp<true, TCTypeList<t_CFromHead, tp_CFromTypes...>, TCTypeList<t_CToHead, tp_CToTypes...>>
+			: public NTraits::TCCompileTimeConstant
+			<
+				bool
+				, NTraits::TCIsConstructorCallableWith<t_CToHead, void (t_CFromHead)>::mc_Value
+				&& TCTypeList_IsConstructibleImp<true, TCTypeList<tp_CFromTypes...>, TCTypeList<tp_CToTypes...> >::mc_Value
+			>
+		{
+		};
+
+		template <>
+		struct TCTypeList_IsConstructibleImp<true, TCTypeList<>, TCTypeList<>>
+			: public NTraits::CCompileTimeTrue
+		{
+		};
+
+	}
+
+	template <typename t_CFromList, typename t_CToList>
+	struct TCTypeList_IsConstructible
+		: public NPrivate::TCTypeList_IsConstructibleImp
+		<
+			TCTypeList_Len<typename NTraits::TCRemoveReference<t_CFromList>::CType>::mc_Value
+			== TCTypeList_Len<typename NTraits::TCRemoveReference<t_CToList>::CType>::mc_Value // Same length
+			, typename TCTypeList_MakePromotedLValueRef<t_CFromList>::CType
+			, t_CToList
+		>
+	{
+		static_assert(!NTraits::TCIsReference<t_CToList>::mc_Value, "To list cannot be reference");
+	};
+
+	/////////////
+	// Type list is assignable
+
+	namespace NPrivate
+	{
+		template <bool t_bSameLength, typename t_CFromList, typename t_CToList>
+		struct TCTypeList_IsAssignableImp : public NTraits::CCompileTimeFalse
+		{
+		};
+
+		template <typename t_CFromHead, typename... tp_CFromTypes, typename t_CToHead, typename... tp_CToTypes>
+		struct TCTypeList_IsAssignableImp<true, TCTypeList<t_CFromHead, tp_CFromTypes...>, TCTypeList<t_CToHead, tp_CToTypes...>>
+			: public NTraits::TCCompileTimeConstant
+			<
+				bool
+				, NTraits::TCIsOperatorCallableWith_Assign<t_CToHead, void (t_CFromHead)>::mc_Value
+				&& TCTypeList_IsAssignableImp<true, TCTypeList<tp_CFromTypes...>, TCTypeList<tp_CToTypes...> >::mc_Value
+			>
+		{
+		};
+
+		template <>
+		struct TCTypeList_IsAssignableImp<true, TCTypeList<>, TCTypeList<>>
+			: public NTraits::CCompileTimeTrue
+		{
+		};
+
+	}
+
+	template <typename t_CFromList, typename t_CToList>
+	struct TCTypeList_IsAssignable
+		: public NPrivate::TCTypeList_IsAssignableImp
+		<
+			TCTypeList_Len<typename NTraits::TCRemoveReference<t_CFromList>::CType>::mc_Value
+			== TCTypeList_Len<typename NTraits::TCRemoveReference<t_CToList>::CType>::mc_Value // Same length
+			, typename TCTypeList_MakePromotedLValueRef<t_CFromList>::CType
+			, t_CToList
+		>
+	{
+		static_assert(!NTraits::TCIsReference<t_CToList>::mc_Value, "To list cannot be reference");
+	};
+}
